ae3e651b86c941027e9050622be72b388b7839a1
eng.ivanrff <eng.ivanrff@gmail.com>
2025-09-02 09:50:41 -0300

    possivel final commit 2

---
+++ API_KEY template.py
@@ -0,0 +1,4 @@
+# Ex.: https://api.themoviedb.org/3/search/movie?api_key=ac1f1cd3ae3c70b82e33d00dcf7f01fe&query=Seppuku&year=1962
+API_KEY = "chave_api_aqui" # API DO TMDB
+# Ex.2: 14537
+OMDB_KEY = "chave_api_aqui" # API DO OMDB
